01b5be700c303824028af52a25eb1963eb274266
kousar-21 <kousarahammed5@gmail.com>
2025-04-04 20:09:24 +0600

    now time is 8:10

---
+++ components/countries/counties.jsx
@@ -0,0 +1,33 @@
+
+
+// normal funtion
+function Countries(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+// Arrow function
+const Countrias = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+// shortcurt with rsc
+import React from 'react';
+
+const counties = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default counties;
